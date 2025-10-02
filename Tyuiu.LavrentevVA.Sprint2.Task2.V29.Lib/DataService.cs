3fa2a0f3b3e3e764a2840f30e0de69afecbb2a57
dimka <dimka556.dim4@gmail.com>
2025-10-02 23:15:10 +0500

    Task2

---
+++ Tyuiu.LavrentevVA.Sprint2.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.LavrentevVA.Sprint2.Task2.V29.Lib
+{
+    public class DataService : ISprint2Task2V29
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if ((x >= 3 && x <= 5 && y >= 3 && y <= 7) ||   
+            (x >= 6 && x <= 8 && y >= 5 && y <= 11) ||    
+            (x >= 9 && x <= 12 && y >= 3 && y <= 7) ||  
+            (x >= 6 && x <= 12 && y >= 8 && y <= 9) ||  
+            (x >= 9 && x <= 10 && y >= 10 && y <= 11))  
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint2.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.LavrentevVA.Sprint2.Task2.V29.Lib;
+namespace Tyuiu.LavrentevVA.Sprint2.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
