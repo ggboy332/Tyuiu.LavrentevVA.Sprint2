7f3ec8232a3017e8bff9dc1cbd2ac8e8b0376cab
dimka <dimka556.dim4@gmail.com>
2025-10-03 00:14:04 +0500

    Task3

---
+++ Tyuiu.LavrentevVA.Sprint2.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.LavrentevVA.Sprint2.Task3.V11.Lib
+{
+    public class DataService : ISprint2Task3V11
+    {
+        public double Calculate(double x)
+        {
+            double y = 0;
+
+            if (x > 0)
+            {
+                y = x + Math.Pow((x - 15) / (x - 19), x);
+            }
+            else if (x == 0)
+            {
+                y = (x * x - Math.Cos(x * x) + 10) / (x * x - Math.Sin(x * x) + 12);
+            }
+            else if (x > -15 && x < 0)
+            {
+                y = Math.Pow(1 + 1 / x * x, x);
+            }
+            else if (x < -15) ;
+            {
+                y = x + 10 * x - 1 / x;
+            }
+
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint2.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.LavrentevVA.Sprint2.Task3.V11.Lib;
+namespace Tyuiu.LavrentevVA.Sprint2.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 10;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+
+        }
+    }
+}
