f179d3a0391d274a69d503694f7761901712659a
dimka <dimka556.dim4@gmail.com>
2025-10-03 01:43:57 +0500

    Task5

---
+++ Tyuiu.LavrentevVA.Sprint2.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,53 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.LavrentevVA.Sprint2.Task5.V1.Lib
+{
+    public class DataService : ISprint2Task5V1
+    {
+        public int FindMonthDaysCount(int value)
+        {
+            int res;
+            switch(value)
+            {
+                case 1:
+                    res = 31;
+                    break;
+                case 2:
+                    res = 28;
+                    break;
+                case 3:
+                    res = 31;
+                    break;
+                case 4:
+                    res = 30;
+                    break;
+                case 5:
+                    res = 31;
+                    break;
+                case 6:
+                    res = 30;
+                    break;
+                case 7:
+                    res = 31;
+                    break;
+                case 8:
+                    res = 31;
+                    break;
+                case 9:
+                    res = 30;
+                    break;
+                case 10:
+                    res = 31;
+                    break;
+                case 11:
+                    res = 30;
+                    break;
+                case 12:
+                    res = 31;
+                    break;
+                default:
+                    throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {value}");
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.LavrentevVA.Sprint2.Task5.V1.Lib;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Tyuiu.LavrentevVA.Sprint2.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+            Console.WriteLine(" Введите номер месяца: ");
+            int numMounth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numMounth < 1) || (numMounth > 12))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Кол-во дней:" + ds.FindMonthDaysCount(numMounth);
+            }
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
