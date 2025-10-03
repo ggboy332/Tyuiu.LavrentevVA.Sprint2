41f55a3666b2e9b71dbc7d4d30d38a33d9af1ef6
dimka <dimka556.dim4@gmail.com>
2025-10-03 20:24:57 +0500

    Task7

---
+++ Tyuiu.LavrentevVA.Sprint2.Task7.V4.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.LavrentevVA.Sprint2.Task7.V8.Lib
+{
+    public class DataService : ISprint2Task7V8
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if (x >= -2 && x <= 3 && y >= x && y <= 4)
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+
+        }
+    }
+}
+
+        {
---
+++ Tyuiu.LavrentevVA.Sprint2.Task7.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.LavrentevVA.Sprint2.Task7.V8.Lib;
+namespace Tyuiu.LavrentevVA.Sprint2.Task7.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double x = 2;
+            double y = 2;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
